bfa7ed0a685ca7b09a54ad9471cc435ce46bb3ce
nazmzadeh <nezaket.mammedzadeh@gmail.com>
2023-12-26 01:25:06 +0400

    cors issue

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const options = {
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  };
-  app.enableCors(options);
+  const app = await NestFactory.create(AppModule);
+  // const options = {
+  //   origin: true,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: 'Content-Type, Accept',
+  // };
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
